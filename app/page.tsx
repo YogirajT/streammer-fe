d3093e68cdc6374de91db2192c73fb585e327edc
Yogi (Linux) <tawdeyogiraj@yahoo.com>
2023-05-23 16:48:18 +0000

    WIP: Menu Sidebar and icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import Image from 'next/image'
 import VideoStream from './video-stream';
 import Dashboard from './components/dashboard';
+import SideBar from './components/sidebar';
+config.autoAddCss = false
 
 export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-0">
+      <SideBar />
       <Dashboard>
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
           <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
