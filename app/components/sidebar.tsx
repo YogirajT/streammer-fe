a995b5023c42ab887b345632fb2b77de1fc32e39
Yogi (Linux) <tawdeyogiraj@yahoo.com>
2023-05-27 18:29:19 +0000

    WIP: Fixing context issues

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,38 +1,44 @@
 import { faPause, faPlay, faRefresh, faRightFromBracket } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useContext } from "react"
+import { AppContext } from "../contexts/AppContext"
 
 
 function SideBar() {
-    return <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-            <ul className="space-y-2 font-medium">
-                <li>
-                    <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                        <FontAwesomeIcon icon={faPlay} />
-                        <span className="flex-1 ml-3 whitespace-nowrap">Start Stream</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                        <FontAwesomeIcon icon={faPause} />
-                        <span className="flex-1 ml-3 whitespace-nowrap">Pause</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                        <FontAwesomeIcon icon={faRefresh} />
-                        <span className="flex-1 ml-3 whitespace-nowrap">Restart</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                        <FontAwesomeIcon icon={faRightFromBracket} />
-                        <span className="flex-1 ml-3 whitespace-nowrap">Sign Out</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </aside>
+
+  const { state } = useContext(AppContext);
+
+  console.log(state)
+  return <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+    <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+      <ul className="space-y-2 font-medium">
+        <li>
+          <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <FontAwesomeIcon icon={faPlay} />
+            <span className="flex-1 ml-3 whitespace-nowrap">Start Stream</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <FontAwesomeIcon icon={faPause} />
+            <span className="flex-1 ml-3 whitespace-nowrap">Pause</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <FontAwesomeIcon icon={faRefresh} />
+            <span className="flex-1 ml-3 whitespace-nowrap">Restart</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <FontAwesomeIcon icon={faRightFromBracket} />
+            <span className="flex-1 ml-3 whitespace-nowrap">Sign Out</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </aside>
 }
 
 
--- app/contexts/AppContext.tsx
+++ app/contexts/AppContext.tsx
@@ -1,26 +1,31 @@
+"use client";
 import React, { useReducer, useState } from 'react';
 
-const AppContext = React.createContext({ sidbarOpen: false });
-
-
 type AppState = {
-  sidbarOpen: boolean
+  sidebarOpen: boolean
 }
 
-const defaultAppState: AppState = { sidbarOpen: false };
+type ProviderProps = {
+  state: AppState,
+  toggleSidebar: () => void
+}
 
-enum AppActionKind {
+const defaultAppState: AppState = { sidebarOpen: false };
+
+const AppContext = React.createContext<ProviderProps>({ state: defaultAppState, toggleSidebar: () => {} });
+
+enum AppActionType {
   SIDEBAR_TOGGLE = 'SIDEBAR_TOGGLE',
 }
 
 interface SidebarToggleAction {
-  type: AppActionKind;
+  type: AppActionType;
 }
 
 const appContextReducer = (state: AppState, action: SidebarToggleAction) => {
   switch (action.type) {
-    case AppActionKind.SIDEBAR_TOGGLE:
-      return { ...state, sidbarOpen: !state.sidbarOpen };
+    case AppActionType.SIDEBAR_TOGGLE:
+      return { ...state, sidebarOpen: !state.sidebarOpen };
     default:
       return state;
   }
@@ -30,8 +35,10 @@ const AppStateProvider = (props: React.PropsWithChildren) => {
 
   const [state, dispatch] = useReducer(appContextReducer, defaultAppState);
 
+  const toggleSidebar = () => dispatch({ type: AppActionType.SIDEBAR_TOGGLE });
+
   return (
-    <AppContext.Provider value={state}>
+    <AppContext.Provider value={{ state, toggleSidebar }}>
       {props.children}
     </AppContext.Provider>
   );
